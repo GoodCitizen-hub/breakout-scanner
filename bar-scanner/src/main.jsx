eb894bdf38d48d5c97105a8537da95fdab99a6c3
GoodCitizen-hub <james.t.jeter@gmail.com>
2026-04-05 19:43:56 -0400

    Create main.jsx

---
+++ bar-scanner/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BarScanner from './BarScanner'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BarScanner />
+  </React.StrictMode>
+)
