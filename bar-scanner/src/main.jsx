095cc02615fdcc124eac10d8bb90e76a32d5d223
GoodCitizen-hub <james.t.jeter@gmail.com>
2026-04-05 20:02:34 -0400

    Update main.jsx

--- bar-scanner/src/main.jsx
+++ bar-scanner/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BarScanner from './BarScanner'
+import BarScanner from '../BarScanner'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
